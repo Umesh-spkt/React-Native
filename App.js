4d48b7fca8afd70c01b2ef4dcc1a468f77c4ce3d
Umesh Sapkota <40801789+Umesh-spkt@users.noreply.github.com>
2021-02-17 16:46:22 +0545

    AQI layout design  started

--- App.js
+++ App.js
@@ -1,57 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View, ImageBackground } from 'react-native';
-import { Ionicons, FontAwesome, AntDesign} from '@expo/vector-icons';
-import { Food } from './components';
+import { Image, StyleSheet, Text, View, StatusBar, ImageBackground, ScrollView, MaskedViewComponent, FlatList} from 'react-native';
+import { Ionicons, FontAwesome, AntDesign} from '@expo/vector-icons'
 
 export default function App() {
-
-  const image ={uri:'https://previews.123rf.com/images/romastudio/romastudio1603/romastudio160300280/54088843-healthy-food-background-studio-photo-of-different-fruits-on-white-wooden-table-high-resolution-produ.jpg'};
   return (
-    <View style={styles.container}>
-        <StatusBar hidden={true} />
-      <ImageBackground source={image} style={styles.image}>
-        <View style={{padding:20}}>
-      
-        <View style={styles.iconbar}>
-          <Ionicons name="cellular" size={24} color="white" />
-          <FontAwesome style={styles.iconmargin} name="wifi" size={24} color="white" />
-          <FontAwesome name="battery-4" size={24} color="white" />
-        </View>
-        <View style={styles.view1}>
-          <Ionicons name="arrow-back" size={30} color="black" />
-          <Text style={{ fontWeight: "bold", fontSize: 30, marginTop: 20 }}>
-            Nepali
-        </Text>
-          <Text style={{ fontSize: 30, marginBottom: 20 }}>
-            Food
-        </Text>
-        </View>
-        </View>
-        </ImageBackground>
+        <View style={styles.container}>
+          <StatusBar backgroundColor="white" barStyle='dark-content'/>
 
-      <View style={styles.view2}>
-        <View style={{flexDirection:'row', justifyContent:'flex-end', marginBottom:20, marginTop:-10}}>
-        <AntDesign name="menu-unfold" size={24} color="red" />
+         <View style={styles.top}>
+           <Text style={styles.title}>AirQuality</Text>
+         </View>
+
+         <View style={{alignItems:'center', paddingVertical:25, flex:1.5}}>
+           <View style={{flexDirection:'row'}}>
+           <FontAwesome name="location-arrow" size={40} color="#53667E" />
+           <Text style={{fontSize:30,color:"#53667E", paddingHorizontal:5}}>Kathmandu</Text>
+           </View>
+           <Text style={{fontSize:15, color:"gray"}}>ktm, Nepal</Text>
+         </View>
+
+         <View style={styles.status}>
+          <View style={{flex:4, flexDirection:'row', justifyContent:'space-evenly', alignItems:'center'}}>
+           <View>
+             <Text>Icon</Text>
+           </View>
+           <View>
+             <Text>AQI</Text>
+           </View>
+           <View >
+             <Text>Category</Text>
+             <Text>pm3.5 value/m3</Text>
+             </View>
+          </View>
+          <View style=
+          {{backgroundColor:"white",flex:3, justifyContent:'space-evenly',
+          flexDirection:'row', alignItems:'center',}}>
+          
+              <View style={{flexDirection:'row'}}>
+              <Text>Icon</Text>
+              <Text>Temperature</Text>
+              </View>
+
+              <View style={{flexDirection:'row'}}>
+                <Text>Icon</Text>
+                <Text>Rain%</Text>
+              </View>
+
+              <View style={{flexDirection:'row'}}>
+                <Text>Icon</Text>
+                <Text>distance</Text>
+              </View>
+
+
+              
+            
+          </View>
         </View>
-      
-       
-        <Food price="$100" fname="Chinese Cuisine" desc="Contrary to popular belief, 
-        Lorem Ipsum is not simply random text. ">https://www.refrigeratedfrozenfood.com/ext/resources/NEW_RD_Website/DefaultImages/default-pasta.jpg?1430942591</Food>
-        <Food price="$200" fname="Nepali Cuisine" desc="Contrary to popular belief, 
-        Lorem Ipsum is not simply random text. ">https://www.refrigeratedfrozenfood.com/ext/resources/NEW_RD_Website/DefaultImages/default-pasta.jpg?1430942591</Food>
-        <Food price="$100" fname="Chinese Cuisine" desc="Contrary to popular belief, 
-        Lorem Ipsum is not simply random text. ">https://www.refrigeratedfrozenfood.com/ext/resources/NEW_RD_Website/DefaultImages/default-pasta.jpg?1430942591</Food>
-        <Food price="$200" fname="Nepali Cuisine" desc="Contrary to popular belief, 
-        Lorem Ipsum is not simply random text. ">https://www.refrigeratedfrozenfood.com/ext/resources/NEW_RD_Website/DefaultImages/default-pasta.jpg?1430942591</Food>
-        <Food price="$200" fname="Nepali Cuisine" desc="Contrary to popular belief, 
-        Lorem Ipsum is not simply random text. ">https://www.refrigeratedfrozenfood.com/ext/resources/NEW_RD_Website/DefaultImages/default-pasta.jpg?1430942591</Food>
-      </View>
+
+        <View style=
+        {{ marginHorizontal:20, flex:3,
+         padding:10, justifyContent:'center'}}>
+        <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+          <View style={{flexDirection:'row'}}>
+          <Text>Picture</Text>
+          <Text> About AQI</Text>
+          </View>
+          <Text>MoreIcon</Text>
+        </View>
+        </View>
 
 
-    </View>
+        <View style={{flex:3, marginHorizontal:20}}>
+          <Text>
+            7 Days Weather forecast
+          </Text>
 
-  );
+        </View>
+
+
+{/* Navigation bar */}
+        <View style={{flex:1, marginHorizontal:20 }} >
+          <FlatList ListFooterComponent={<Text>Navigation to home, data and predictAQI</Text>}/>
+
+        </View>
+
+
+        </View>
+         );
 }
 
 
@@ -59,37 +94,30 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    justifyContent: 'space-evenly',
-    padding: 10
+    backgroundColor: "#fff",
+    paddingVertical:5,
   },
-  view2: {
-    flex: 8,
-    borderRadius:30,
-    marginTop:-22,
-    backgroundColor:"#fff",
-    paddingHorizontal:20,
-    paddingVertical:40
+  title:{
+    color:'#4783B7',
+    fontSize:32,
+    fontWeight:'bold'
 
-  },
-  iconbar: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    marginBottom: 20
-  },
-  view1: {
-    flex: 2.5
-  },
-  iconmargin: {
-    marginHorizontal: 5
-  },
-  top: {
-    flex: 2.5,
-  },
-  image: {
-    width:'100%',
-    height:250
+},
+top:{
+flex:1,
+alignItems:'center',
+borderBottomWidth:1,
+borderColor:'#d3d3d3',
+paddingVertical:20,
+paddingHorizontal:15
+},
+status:{
+  marginHorizontal:20,
+  borderRadius:5,
+  backgroundColor:"#FE7777",
+  flex:5,
+  justifyContent:'space-between',
+  elevation:5
 
-  }
-
-})
+}
+});
